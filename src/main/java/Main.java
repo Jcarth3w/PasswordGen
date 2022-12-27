f5f0c0a6099483040302563d42feb69083badda0
Jack <jdcarthew@bsu.edu>
2022-12-26 19:06:50 -0800

    Changed names

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,17 @@
 import java.util.Scanner;
 
+
 public class Main
 {
     public static PasswordGenerator generator = new PasswordGenerator();
     public static void main(String[] args)
     {
-        Scanner bruh = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         int desiredLength;
         System.out.println("Enter your desired length for your password");
-        desiredLength = bruh.nextInt();
+        desiredLength = scan.nextInt();
         Password newPassword = generator.generatePassword(desiredLength);
         System.out.println(newPassword.getValue());
     }
+
 }
